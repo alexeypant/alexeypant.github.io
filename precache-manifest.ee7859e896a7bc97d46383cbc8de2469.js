2c937587f802ed36aebfdd0e5be870bca535704b
alexeypant <alexeypant@gmail.com>
2021-05-22 18:13:41 +0300

    initial commit

---
+++ precache-manifest.ee7859e896a7bc97d46383cbc8de2469.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b306d4ce85d9807c24e19de398d74bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68ccb57d2d298902087d",
+    "url": "/static/css/2.8320ddf7.chunk.css"
+  },
+  {
+    "revision": "c549e2b3d9e48d0cbb12",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "68ccb57d2d298902087d",
+    "url": "/static/js/2.703af419.chunk.js"
+  },
+  {
+    "revision": "c549e2b3d9e48d0cbb12",
+    "url": "/static/js/main.f6662e90.chunk.js"
+  },
+  {
+    "revision": "68fe72aaac6016f55a7c",
+    "url": "/static/js/runtime-main.620c772e.js"
+  },
+  {
+    "revision": "7f07bf3484713c079f278f2c1852451b",
+    "url": "/static/media/kupol-base.7f07bf34.jpg"
+  },
+  {
+    "revision": "b3955151f7f4d2e03d9d8e61c35854b3",
+    "url": "/static/media/kupol-inside-wall-close.b3955151.jpeg"
+  },
+  {
+    "revision": "34995d3ecb4f398a5bc839e758ee3aaf",
+    "url": "/static/media/kupol-inside.34995d3e.jpeg"
+  },
+  {
+    "revision": "a3483e0b74cbadd36faead21bf43a5a4",
+    "url": "/static/media/kupol-outside-view-full.a3483e0b.jpeg"
+  },
+  {
+    "revision": "2f00d5622695ceaa7f906563f11675ee",
+    "url": "/static/media/kupol-outside-view-long.2f00d562.jpeg"
+  },
+  {
+    "revision": "576affcd315df5bb17c65582d693764c",
+    "url": "/static/media/kupol-outside-view.576affcd.jpeg"
+  },
+  {
+    "revision": "7b0fa67c2df01f6ecf6ac1db193ed2f5",
+    "url": "/static/media/kupol-top.7b0fa67c.jpeg"
+  },
+  {
+    "revision": "d99d3ed114cdabea8042c5acaa46281b",
+    "url": "/static/media/kupol-wall-internal.d99d3ed1.jpeg"
+  },
+  {
+    "revision": "207a067311a01dda1db8a83d780a5a2a",
+    "url": "/static/media/kupol-wall-outside.207a0673.jpeg"
+  }
+]);
